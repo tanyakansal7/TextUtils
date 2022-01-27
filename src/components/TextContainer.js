c02cbda66a0064b916c342a23445b1a30d7f3396
tanyakansal7 <you@example.com>
2022-01-27 22:25:07 +0530

    corrected a few flaws

--- src/components/TextContainer.js
+++ src/components/TextContainer.js
@@ -16,6 +16,9 @@ export default function TextContainer(props) {
         let newText=text.toLowerCase();
         setText(newText)
     }
+    const handleCopyClick=()=>{
+        navigator.clipboard.writeText(text);
+    }
     const handleChange=(event)=>{
     setText(event.target.value)
     }
@@ -34,12 +37,13 @@ export default function TextContainer(props) {
 </div>
 <button type="button" onClick={handleUpClick} className="btn btn-primary mx-2">UpperCase</button>
 <button type="button" onClick={handleLoClick} className="btn btn-primary mx-2">LowerCase</button>
-<button type="button" onClick={handleClickClear} className="btn btn-secondary mx-2">Clear</button>
 <button type="button" onClick={handleReverseClick} className="btn btn-danger mx-2">Reverse</button>
+<button type="button" onClick={handleCopyClick} className="btn btn-warning mx-2">Copy</button>
+<button type="button" onClick={handleClickClear} className="btn btn-secondary mx-2">Clear</button>
 <div className="container my-3" >
     <h3>Your Text Summary</h3>
-    <p className='text-info'>{text.split(" ").length-1} words {text.length} characters</p>
-    <p className='text-danger'>{0.008*(text.split(" ").length-1)} minutes to read</p>
+    <p className='text-info'>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words {text.length} characters</p>
+    <p className='text-danger'>{0.008*(text.split(" ").filter((element)=>{return element.length!==0}).length)} minutes to read</p>
     <h3>Preview</h3>
     <p className='text-info'>{text}</p>
 </div>
